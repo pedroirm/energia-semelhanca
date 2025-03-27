cdc23bd73f177e2ebddaada7ecfc98f18a2e9e84
Pedro Icaro <pedroicaro35@gmail.com>
2025-03-27 19:23:34 -0300

    up

---
+++ src/app/lead/page.tsx
@@ -0,0 +1,9 @@
+export const runtime = "edge";
+
+export default function LeadPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Página de Lead</h1>
+    </div>
+  );
+}
